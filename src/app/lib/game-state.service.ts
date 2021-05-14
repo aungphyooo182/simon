55f867ed11c1c862f830940439a3c2ac91f6e246
Aung Phyo Oo <aungphyooo@amdon.com>
2021-05-14 16:55:14 +0630

    assets change

--- src/app/lib/game-state.service.ts
+++ src/app/lib/game-state.service.ts
@@ -4,15 +4,15 @@ import { environment } from 'src/environments/environment';
 import { START_COUNT, COLORS } from '../models/constant';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameStateService {
-  public simon : string[] = [];
-  public player : string[] = [];
+  public simon: string[] = [];
+  public player: string[] = [];
   public count: number;
-  public showWinnerText : boolean;
-  public showErrorText : boolean;
-  public state = new Subject<any> (); // subjecy is a pratial observable
+  public showWinnerText: boolean;
+  public showErrorText: boolean;
+  public state = new Subject<any>(); // subjecy is a pratial observable
 
   public finishedLoop = false;
   public redAudio = new Audio();
@@ -20,17 +20,21 @@ export class GameStateService {
   public blueAudio = new Audio();
   public yellowAudio = new Audio();
   public soundEffects = [
-    this.redAudio, this.greenAudio, this.blueAudio, this.yellowAudio
+    this.redAudio,
+    this.greenAudio,
+    this.blueAudio,
+    this.yellowAudio,
   ];
   public debugMode = environment.showDebug;
   // src/app/services/apo-game-state.service
   constructor() {
     this.debugMode = false; //testing
     this.count = START_COUNT;
-    this.redAudio.src = '../../assets/sounds/sfx-animal-dog.mp3';
-    this.greenAudio.src = '../../assets/sounds/sfx-animal-kitten.mp3';
-    this.blueAudio.src = '../../assets/sounds/sfx-animal-pig.mp3';
-    this.yellowAudio.src = '../../assets/sounds/sfx-animal-sheep.mp3';
+
+    this.redAudio.src = './assets/sounds/sfx-animal-dog.mp3';
+    this.greenAudio.src = './assets/sounds/sfx-animal-kitten.mp3';
+    this.blueAudio.src = './assets/sounds/sfx-animal-pig.mp3';
+    this.yellowAudio.src = './assets/sounds/sfx-animal-sheep.mp3';
     this.redAudio.load();
     this.greenAudio.load();
     this.blueAudio.load();
@@ -42,19 +46,20 @@ export class GameStateService {
     this.yellowAudio.volume = this.debugMode ? 0.1 : 1;
   }
 
-  private get randomColor() : string {
-    return COLORS[Math.floor(Math.random()*4)];
+  private get randomColor(): string {
+    return COLORS[Math.floor(Math.random() * 4)];
   }
 
-  appendSimon(increment:boolean = false) : void {
+  appendSimon(increment: boolean = false): void {
     this.simon.push(this.randomColor); //don't need () since get method
     if (increment) {
       this.count++;
       this.setState();
-    };
+    }
   }
 
-  generateSimon() : void { //first
+  generateSimon(): void {
+    //first
     console.log('generateSimon');
     this.loopIndex = 0;
     this.simon = [];
@@ -73,13 +78,14 @@ export class GameStateService {
       simon: this.simon,
       count: this.count,
       finishedLoop: this.finishedLoop,
-      showWinnerText : this.showWinnerText,
-      showErrorText : this.showErrorText
+      showWinnerText: this.showWinnerText,
+      showErrorText: this.showErrorText,
     });
     console.log('set state');
   }
 
-  restartSimon() : void { //checked
+  restartSimon(): void {
+    //checked
     this.player = [];
     this.showWinnerText = false;
     this.showErrorText = false;
@@ -87,7 +93,7 @@ export class GameStateService {
     this.generateSimon();
   }
 
-  playeSound(val: number){
+  playeSound(val: number) {
     console.log(val);
     this.soundEffects[val].play();
     // this.soundEffects[val].pause;
@@ -102,47 +108,49 @@ export class GameStateService {
     this.player.push(val); //push val to player array
     this.playeSound(COLORS[val]);
     // this.compareSimon().then(this.isComplete()); //check if correct
-    this.compareSimon().subscribe(data=>{
+    this.compareSimon().subscribe((data) => {
       if (data) {
         console.log('check');
         this.checkComplete();
-      }else {
-
+      } else {
       }
     });
   }
 
-  compareSimon(): Observable<any>{
+  compareSimon(): Observable<any> {
     for (let i = 0; i < this.player.length; i++) {
-      if(this.player[i] !== this.simon[i]) {
+      if (this.player[i] !== this.simon[i]) {
         this.player = [];
         console.log('wrong');
         setTimeout(() => {
           this.showErrorText = true;
-        this.setState();
-        return of (false);
+          this.setState();
+          return of(false);
         }, 100);
-      }
-      else {
+      } else {
         console.log('correct');
       }
     }
     return of(true);
   }
 
-  showWinner(){
+  showWinner() {
     this.showWinnerText = true;
     this.setState();
   }
 
-  checkComplete(){
-    console.log(this.player.length,this.simon.length,this.player.length === this.simon.length)
+  checkComplete() {
+    console.log(
+      this.player.length,
+      this.simon.length,
+      this.player.length === this.simon.length
+    );
     if (this.player.length === this.simon.length) {
-      console.log('update')
+      console.log('update');
       // I don't want to update asap and show message
       this.showWinner();
       // this.updateGame();
-    }else {
+    } else {
       // setTimeout(() => {
       //   this.index = 0;
       //   this.setRing(true);
@@ -151,7 +159,7 @@ export class GameStateService {
     }
   }
 
-  public loopIndex=0;
+  public loopIndex = 0;
 
   loop() {
     this.finishedLoop = false;
@@ -159,8 +167,8 @@ export class GameStateService {
       console.log('loop');
       this.clearAll();
       setTimeout(() => {
-        var id = '#'+this.simon[this.loopIndex];
-        console.log(id+' is added');
+        var id = '#' + this.simon[this.loopIndex];
+        console.log(id + ' is added');
         var button = document.querySelectorAll(id)[0];
         button.classList.add('active');
         this.playeSound(COLORS[this.simon[this.loopIndex]]);
@@ -170,7 +178,7 @@ export class GameStateService {
           this.loop();
         }, 1000);
       }, 500);
-    }else {
+    } else {
       this.clearAll();
       this.loopIndex = 0;
       this.finishedLoop = true;
@@ -178,18 +186,18 @@ export class GameStateService {
     }
   }
 
-  clearAll(){
-    for (let i=0; i<4; i++){
-      var id = '#'+COLORS[i];
-        var button = document.querySelectorAll(id)[0];
-        button.classList.remove('active');
+  clearAll() {
+    for (let i = 0; i < 4; i++) {
+      var id = '#' + COLORS[i];
+      var button = document.querySelectorAll(id)[0];
+      button.classList.remove('active');
     }
   }
 
-  clearOthers(color:string) {
-    for (let i=0;i<4; i++) {
-      if(color !== COLORS[i]) {
-        var id = '#'+COLORS[i];
+  clearOthers(color: string) {
+    for (let i = 0; i < 4; i++) {
+      if (color !== COLORS[i]) {
+        var id = '#' + COLORS[i];
         var button = document.querySelectorAll(id)[0];
         button.classList.remove('active');
       }
@@ -199,12 +207,12 @@ export class GameStateService {
   updateGame() {
     this.showWinnerText = false;
     console.log('updateGame');
-      this.appendSimon(true);
-      this.loop();
-      this.player = [];
+    this.appendSimon(true);
+    this.loop();
+    this.player = [];
   }
 
-  tryAgain(){
+  tryAgain() {
     this.showErrorText = false;
     this.setState();
     this.loopIndex = 0;
