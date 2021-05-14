8bd9f9088fbd521a6c05e60ee730883462cebc6a
Aung Phyo Oo <aungphyooo@amdon.com>
2021-05-14 12:02:27 +0630

    og image

--- src/app/game/presentation/game/game-controller.component.ts
+++ src/app/game/presentation/game/game-controller.component.ts
@@ -1,149 +1,174 @@
-import {Inject, Component, HostListener,OnInit} from '@angular/core';
-import {BusinessLogicRequirements, BusinessRequirementsInjectionToken} from '../business-logic.requirements';
-import {GameStore} from './game.store'
+import { Inject, Component, HostListener, OnInit } from '@angular/core';
+import {
+  BusinessLogicRequirements,
+  BusinessRequirementsInjectionToken,
+} from '../business-logic.requirements';
+import { GameStore } from './game.store';
 import { environment } from 'src/environments/environment';
 import { GameStateService } from 'src/app/lib/game-state.service';
 import { Router } from '@angular/router';
 @Component({
-    selector: 'app-game',
-    templateUrl: './game-controller.component.html',
-    styleUrls: ['./game-controller.component.css']
+  selector: 'app-game',
+  templateUrl: './game-controller.component.html',
+  styleUrls: ['./game-controller.component.css'],
 })
 export class GameControllerComponent {
-    constructor(
-        @Inject(BusinessRequirementsInjectionToken) private business: BusinessLogicRequirements,
-        private store: GameStore,
-        private game: GameStateService,
-        private router: Router
-    ) {}
+  constructor(
+    @Inject(BusinessRequirementsInjectionToken)
+    private business: BusinessLogicRequirements,
+    private store: GameStore,
+    private game: GameStateService,
+    private router: Router
+  ) {}
 
-   
+  count: number;
+  ring: boolean;
+  state: any;
+  simonArray: string[] = [];
+  finishedLoop: boolean = false;
+  supportOrientation: boolean = true;
+  showWinnerText: boolean = false;
+  showErrorText: boolean = false;
+  showDebug = environment.showDebug;
+  previousSupportOrientation: boolean = false;
 
-    count: number;
-    ring: boolean;
-    state: any;
-    simonArray: string[]=[];
-    finishedLoop: boolean = false;
-    supportOrientation: boolean = true;
-    showWinnerText: boolean = false;
-    showErrorText: boolean = false;
-    showDebug = environment.showDebug;
-    previousSupportOrientation : boolean = false;
+  public username = localStorage.getItem('username')
+    ? localStorage.getItem('username')
+    : null;
+  public showProfileBox = false;
+  public level = 1;
+  public winnerPopup = {
+    text: [
+      {
+        value: 'You win this time!!!',
+        class: 'win-text',
+      },
+      {
+        value: 'Your can memorize ' + this.level + ' items.',
+        class: 'win-text',
+      },
+    ],
+    btn: [
+      {
+        text: 'Continue',
+        class: 'continue-btn',
+      },
+      {
+        text: 'Reset',
+        class: 'continue-btn',
+      },
+    ],
+  };
 
-    public username = localStorage.getItem('username') ? localStorage.getItem('username') : null;
-    public showProfileBox = false;
-    public level = 1;
-    public winnerPopup = {
-      text : [
-        {
-          value: "You win this time!!!",
-          class: "win-text"
-        },{
-          value: "Your can memorize "+ this.level+" items.",
-          class: "win-text"
-        }
-      ],
-      btn : [
-        {
-          text: "Continue",
-          class: "continue-btn"
-        },{
-          text: "Reset",
-          class: "continue-btn"
-        }
-      ]
-    };
+  public wrongGuessPopup = {
+    text: [
+      {
+        value: 'Your guess is wrong!!!',
+        class: 'win-text',
+      },
+    ],
+    btn: [
+      {
+        text: 'Try again',
+        class: 'continue-btn',
+      },
+      {
+        text: 'Reset',
+        class: 'continue-btn',
+      },
+    ],
+  };
+  public startGame = false;
 
-    public wrongGuessPopup = {
-      text : [
-        {
-          value: "Your guess is wrong!!!",
-          class: "win-text"
-        }
-      ],
-      btn : [
-        {
-          text: "Try again",
-          class: "continue-btn"
-        },{
-          text: "Reset",
-          class: "continue-btn"
-        }
-      ]
-    };
-
-    @HostListener("window:resize") updateOrientatioState() {
-      console.log('ori')
-      if (window.innerHeight > window.innerWidth) {
+  @HostListener('window:resize') updateOrientatioState() {
+    console.log('ori');
+    if (window.innerHeight > window.innerWidth) {
+      this.supportOrientation = true;
+      if (!this.previousSupportOrientation && this.supportOrientation) {
+        this.game.generateSimon();
+      }
+      this.previousSupportOrientation = this.supportOrientation;
+    } else {
+      if (window.innerHeight >= 600) {
         this.supportOrientation = true;
         if (!this.previousSupportOrientation && this.supportOrientation) {
           this.game.generateSimon();
         }
         this.previousSupportOrientation = this.supportOrientation;
       } else {
-        if (window.innerHeight >= 600) {
-          this.supportOrientation = true;
-          if (!this.previousSupportOrientation && this.supportOrientation) {
-            this.game.generateSimon();
-          }
-          this.previousSupportOrientation = this.supportOrientation;
-        } else {
-          this.supportOrientation = false;
-        }
+        this.supportOrientation = false;
       }
     }
+  }
 
-    ngOnInit(): void {
-      this.game.state.subscribe(state=>{
-        console.log(state);
-        this.state = state;
-        this.count = state.count; //for show
-        this.simonArray = state.simon;
-        this.level = this.simonArray.length;
-        this.winnerPopup.text[1].value = "Your can memorize "+ this.level+" items.";
-        this.finishedLoop = state.finishedLoop;
-        this.showWinnerText = state.showWinnerText;
-        this.showErrorText = state.showErrorText;
-      });
-      this.updateOrientatioState();
+  ngOnInit(): void {
+    // this.game.state.subscribe(state=>{
+    //   console.log(state);
+    //   this.state = state;
+    //   this.count = state.count; //for show
+    //   this.simonArray = state.simon;
+    //   this.level = this.simonArray.length;
+    //   this.winnerPopup.text[1].value = "Your can memorize "+ this.level+" items.";
+    //   this.finishedLoop = state.finishedLoop;
+    //   this.showWinnerText = state.showWinnerText;
+    //   this.showErrorText = state.showErrorText;
+    // });
+    // this.updateOrientatioState();
+  }
+
+  startGameClicked() {
+    console.log('start game');
+    this.startGame = true;
+    this.game.state.subscribe((state) => {
+      console.log(state);
+      this.state = state;
+      this.count = state.count; //for show
+      this.simonArray = state.simon;
+      this.level = this.simonArray.length;
+      this.winnerPopup.text[1].value =
+        'Your can memorize ' + this.level + ' items.';
+      this.finishedLoop = state.finishedLoop;
+      this.showWinnerText = state.showWinnerText;
+      this.showErrorText = state.showErrorText;
+    });
+    this.updateOrientatioState();
+  }
+
+  playerGuess(e: string) {
+    console.log(e, 'player guess');
+    if (e !== 'white' && this.finishedLoop) {
+      this.game.playerGuess(e);
+    } else {
+      this.game.restartSimon();
     }
+  }
 
-    playerGuess(e: string) {
-      console.log(e,'player guess');
-      if (e !== 'white' && this.finishedLoop) {
-        this.game.playerGuess(e);
-      } else {
-        this.game.restartSimon();
-      }
-    }
+  tryAgain() {
+    this.game.tryAgain();
+  }
 
-    tryAgain(){
-      this.game.tryAgain();
-    }
+  updateGame() {
+    // sent max times to api
+    this.game.updateGame();
+  }
 
-    updateGame() {
-      // sent max times to api
-      this.game.updateGame();
-    }
+  showProfile() {
+    this.showProfileBox = true;
+  }
 
-    showProfile(){
-      this.showProfileBox = true;
-    }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['']);
+  }
 
-    logout(){
-      localStorage.clear();
-      this.router.navigate(['']);
-    }
+  clickedWinnerPopup(index) {
+    console.log(index);
+    if (index == 0) this.game.updateGame();
+    else this.playerGuess('white');
+  }
 
-    clickedWinnerPopup(index){
-      console.log(index);
-      if (index == 0) this.game.updateGame();
-      else this.playerGuess('white');
-    }
-
-    clickedwrongGuessPopup(index){
-      if (index == 0) this.game.tryAgain();
-      else this.playerGuess('white');
-    }
-
+  clickedwrongGuessPopup(index) {
+    if (index == 0) this.game.tryAgain();
+    else this.playerGuess('white');
+  }
 }
--- src/app/home/presentation/shared-components/card/card.component.ts
+++ src/app/home/presentation/shared-components/card/card.component.ts
@@ -1,97 +1,99 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import {CardStore} from './card.store'
+import { CardStore } from './card.store';
 
 @Component({
-    selector: 'app-card',
-    templateUrl: './card.component.html',
-    styleUrls: ['./card.component.css']
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit{
+export class CardComponent implements OnInit {
   public form = this.fb.group({
-    username: this.fb.control("", [Validators.required]),
-    password: this.fb.control("", [Validators.required,Validators.minLength(8)])
+    username: this.fb.control('', [Validators.required]),
+    password: this.fb.control('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
   });
 
-  constructor(private store: CardStore,private fb: FormBuilder) {}
+  constructor(private store: CardStore, private fb: FormBuilder) {}
 
-  @Output() outputRegister = new EventEmitter ();
-  @Output() outputLogin = new EventEmitter ();
+  @Output() outputRegister = new EventEmitter();
+  @Output() outputLogin = new EventEmitter();
 
-  public usernameClass = "signup-input";
-  public passwordClass = "signup-input";
+  public usernameClass = 'signup-input';
+  public passwordClass = 'signup-input';
   public usernameError = false;
-  public togglePassword = "password";
-  public eyeOpenUrl = "./assets/images/eye_open.svg";
-  public eyeCloseUrl = "./assets/images/eye_close.svg";
+  public togglePassword = 'password';
+  public eyeOpenUrl = './assets/images/eye_open.svg';
+  public eyeCloseUrl = './assets/images/eye_close.svg';
   public clickBtn = false;
   public clickedEyeOpen = false;
   public loading = false;
   public username;
   public password;
 
-  public showLoginForm = false;
+  public showLoginForm = true;
 
-  ngOnInit(){
+  ngOnInit() {
     this.form.valueChanges.subscribe((value) => {
       this.clickBtn = false;
-      this.usernameClass = "signup-full";
-      this.passwordClass = "signup-full";
+      this.usernameClass = 'signup-full';
+      this.passwordClass = 'signup-full';
       this.usernameError = false;
     });
   }
 
   blurInput($event) {
-    console.log("get blur event");
+    console.log('get blur event');
   }
 
-  showLogin(){
+  showLogin() {
     this.showLoginForm = true;
   }
 
-  showSignup(){
+  showSignup() {
     this.showLoginForm = false;
   }
 
-  signup(){
+  signup() {
     this.clickBtn = true;
-    console.log('clicked')
+    console.log('clicked');
     // this.existUserName = true;
     if (this.form.valid) {
       this.username = this.form.get('username').value.trim();
       this.password = this.form.get('password').value.trim();
       var registerObject = {
-        'username' : this.username,
-        'password' : this.password
-      }
+        username: this.username,
+        password: this.password,
+      };
       // this.shared.setRegisterData(registerObject);
       // this.nextOutput.emit(registerObject);
       console.log('next');
       this.outputRegister.emit(registerObject);
-    }else{
-      console.log(this.form.errors)
-      this.usernameClass = "input-err";
-      this.passwordClass = "input-err";
+    } else {
+      console.log(this.form.errors);
+      this.usernameClass = 'input-err';
+      this.passwordClass = 'input-err';
     }
-   }
+  }
 
-   login(){
+  login() {
     this.clickBtn = true;
-    console.log('clicked')
+    console.log('clicked');
     // this.existUserName = true;
     if (this.form.valid) {
       this.username = this.form.get('username').value.trim();
       this.password = this.form.get('password').value.trim();
       var loginObject = {
-        'username' : this.username,
-        'password' : this.password
-      }
+        username: this.username,
+        password: this.password,
+      };
       console.log('next');
       this.outputLogin.emit(loginObject);
-    }else{
-      this.usernameClass = "input-err";
-      this.passwordClass = "input-err";
+    } else {
+      this.usernameClass = 'input-err';
+      this.passwordClass = 'input-err';
     }
-   }
-
+  }
 }
